dc8722fac2ca544dd64f2844ab167ea1e31037a1
Ducky <mail@ducky.ws>
2014-09-25 12:18:53 +0100

    Plugins

---
+++ IRCBot/Plugins/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRCBot.Plugins
+{
+	class HelloWorld
+	{
+		public static void Main(string host, string chan, string says, string cmd, string msg)
+		{
+			Sharpie.writer.WriteLine("PRIVMSG " + chan + " :" + says + "Hello, world!");
+		}
+	}
+}
---
+++ IRCBot/Plugins/Version.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRCBot.Plugins
+{
+	class Version
+	{
+		public static void Main(string host, string chan, string says, string cmd, string msg)
+		{
+			Sharpie.writer.WriteLine("PRIVMSG " + chan + " :" + says + "Sharpie, v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version + "[http://github.com/electricduck/sharpie]");
+		}
+	}
+}
