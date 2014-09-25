00e1a65d83a3d68514a783eb80f45caee21248ef
Ducky <mail@ducky.ws>
2014-09-25 12:18:32 +0100

    Plugins now located in separate file. Allow for hardcoded connection variables. Other nicities.

--- IRCBot/Program.cs
+++ IRCBot/Program.cs
@@ -4,130 +4,156 @@ using System.Net.Sockets;
 using System.IO;
 using System.Threading;
 
-/* 
-* This program establishes a connection to irc server and joins a channel. Thats it.
-*
-* Coded by Pasi Havia 17.11.2001 http://koti.mbnet.fi/~curupted
-*
-* Updated / fixed by Blake 09.10.2010
-*/
-
-class IrcBot
+namespace IRCBot
 {
-	// IRC server to connect 
-	public static string SERVER = "irc.subluminal.net";
-
-	// IRC server's port (6667 is default port)
-	private static int PORT = 6667;
-
-	// User information defined in RFC 2812 (Internet Relay Chat: Client Protocol) is sent to irc server
-	private static string USER = "USER Sharpie 0 * :Sharpie";
-
-	// Bot's nickname
-	private static string NICK = "Sharpie";
-
-	// Channel to join
-	private static string CHANNEL = "#sharpie";
-
-	// And here's where the magic happens...
-
-	public static StreamWriter writer;
-	static void Main(string[] args)
+	class Sharpie
 	{
-		NetworkStream stream;
-		TcpClient irc;
-		string inputLine;
-		StreamReader reader;
-
-		try
+		public static StreamWriter writer;
+		static void Main(string[] args)
 		{
-			irc = new TcpClient(SERVER, PORT);
-			stream = irc.GetStream();
-			reader = new StreamReader(stream);
-			writer = new StreamWriter(stream);
-			writer.WriteLine("NICK " + NICK);
-			writer.Flush();
-			writer.WriteLine(USER);
-			writer.Flush();
+			// Hardcoded Connection
+			var HardCodedConnection = true;
+			var server = "irc.subluminal.net";
+			var port = 6667;
+			var nick = "Sharpie";
+			var channel = "#test";
 
-			Console.WriteLine("Sharpie (v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version + ")");
-			Console.WriteLine("---");
-
-			while (true)
+			Status.Welcome();
+			Line.Single();
+			if (HardCodedConnection == false)
 			{
-				while ((inputLine = reader.ReadLine()) != null)
-				{
-					Console.WriteLine("<-" + inputLine);
-
-					// Split the lines sent from the server by spaces. This seems the easiest way to parse them.
-					string[] splitInput = inputLine.Split(new Char[] { ' ' });
-
-					if (splitInput[0] == "PING")
-					{
-						string PongReply = splitInput[1];
-						//Console.WriteLine("->PONG " + PongReply);
-						writer.WriteLine("PONG " + PongReply);
-						writer.Flush();
-						continue;
-					}
-
-					switch (splitInput[1])
-					{
-						// This is the 'raw' number, put out by the server. Its the first one
-						// so I figured it'd be the best time to send the join command.
-						// I don't know if this is standard practice or not.
-						case "001":
-							string JoinString = "JOIN " + CHANNEL;
-							writer.WriteLine(JoinString);
-							writer.Flush();
-							break;
-						case "PRIVMSG":
-							var host = splitInput[0].Substring(1);
-							var chan = splitInput[2];
-							var msg = splitInput[3].Substring(1);
-
-							switch (msg)
-							{
-								case "#hello":
-									writer.WriteLine("PRIVMSG " + chan +  " :" + "Hello, world!");
-									writer.Flush();
-									break;
-								case "#version":
-									writer.WriteLine("PRIVMSG " + chan + " :" + "v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
-									writer.Flush();
-									break;
-								case "#join":
-									writer.WriteLine("JOIN " + msg);
-									writer.Flush();
-									break;
-								default:
-									break;
-							}
-
-							//Console.WriteLine("! Host:    " + splitInput[0]);
-							//Console.WriteLine("! Channel: " + splitInput[2]);
-							//Console.WriteLine("! Message: " + splitInput[3].Substring(1));
-							writer.Flush();
-							break;
-						default:
-							break;
-					}
-
-				}
-
-				// Close all streams
-				writer.Close();
-				reader.Close();
-				irc.Close();
+				Status.Input("Server:  ");
+				server = Console.ReadLine();
+				Status.Input("Port:    ");
+				port = Convert.ToInt32(Console.ReadLine());
+				Status.Input("Nick:    ");
+				nick = Console.ReadLine();
+				Status.Input("Channel: ");
+				channel = Console.ReadLine();
 			}
-		}
-		catch (Exception e)
-		{
-			// Show the exception, sleep for a while and try to establish a new connection to irc server
-			Console.WriteLine(e.ToString());
-			Thread.Sleep(5000);
-			string[] argv = { };
-			Main(argv);
+			else
+			{
+				Status.OK("Using hardcoded connection settings");
+			}
+			Line.Double();
+
+			Status.Do("Initalizing");
+
+			var SERVER = server;
+			var PORT = port;
+			var PORTString = port.ToString();
+			var USER = "USER Sharpie 0 * :Sharpie";
+			var NICK = nick;
+			var CHANNEL = channel;
+
+			NetworkStream stream;
+			TcpClient irc;
+			string inputLine;
+			StreamReader reader;
+
+			try
+			{
+				Status.Do("Connecting to IRC: '" + SERVER + ":" + PORTString + "'");
+				irc = new TcpClient(SERVER, PORT);
+				stream = irc.GetStream();
+				reader = new StreamReader(stream);
+				writer = new StreamWriter(stream);
+				Status.Do("Setting Nick: '" + NICK + "'");
+				writer.WriteLine("NICK " + NICK);
+				writer.Flush();
+				writer.WriteLine(USER);
+				writer.Flush();
+
+				while (true)
+				{
+					while ((inputLine = reader.ReadLine()) != null)
+					{
+						Status.SendIn(inputLine);
+
+						// Split the lines sent from the server by spaces. This seems the easiest way to parse them.
+						string[] splitInput = inputLine.Split(new Char[] { ' ' });
+
+						if (splitInput[0] == "PING")
+						{
+							string PongReply = splitInput[1];
+							//Console.WriteLine("->PONG " + PongReply);
+							writer.WriteLine("PONG " + PongReply);
+							writer.Flush();
+							continue;
+						}
+
+						switch (splitInput[1])
+						{
+							// This is the 'raw' number, put out by the server. Its the first one
+							// so I figured it'd be the best time to send the join command.
+							// I don't know if this is standard practice or not.
+							case "001":
+								string JoinString = "JOIN " + CHANNEL;
+								Status.Do("Joining Channel: '" + CHANNEL + "'");
+								writer.WriteLine(JoinString);
+								writer.Flush();
+								break;
+							case "PRIVMSG":
+								var host = splitInput[0].Substring(1);
+								var chan = splitInput[2];
+								var cmd = splitInput[3].Substring(1);
+								var says = "";
+								var msg = "";
+								try
+								{
+									msg = splitInput[4];
+								}
+								catch
+								{
+									msg = "";
+								}
+
+								switch (cmd)
+								{
+									case "#hello":
+										Status.Do("Executing: 'HelloWorld'");
+										Plugins.HelloWorld.Main(host, chan, says, cmd, msg);
+										writer.Flush();
+										break;
+									case "#version":
+										Status.Do("Executing: 'Version'");
+										Plugins.Version.Main(host, chan, says, cmd, msg);
+										writer.Flush();
+										break;
+									case "#join":
+										writer.WriteLine("JOIN " + msg);
+										writer.Flush();
+										break;
+									default:
+										break;
+								}
+
+								//Console.WriteLine("! Host:    " + splitInput[0]);
+								//Console.WriteLine("! Channel: " + splitInput[2]);
+								//Console.WriteLine("! Message: " + splitInput[3].Substring(1));
+								writer.Flush();
+								break;
+							default:
+								break;
+						}
+
+					}
+
+					// Close all streams
+					writer.Close();
+					reader.Close();
+					irc.Close();
+				}
+			}
+			catch (Exception e)
+			{
+				// Show the exception, sleep for a while and try to establish a new connection to irc server
+				Console.WriteLine(e.ToString());
+				Thread.Sleep(5000);
+				string[] argv = { };
+				Main(argv);
+			}
+
 		}
 
 	}
---
+++ IRCBot/Utilities.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRCBot
+{
+	public class Utilities
+	{
+		
+	}
+	public class Status
+	{
+		public static void Welcome()
+		{
+			Console.ForegroundColor = ConsoleColor.Black;
+			Console.BackgroundColor = ConsoleColor.Yellow;
+			Console.Write("Sharpie");
+			Console.BackgroundColor = ConsoleColor.DarkYellow;
+			Console.Write(System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+			Console.ResetColor();
+			Console.Write(Environment.NewLine);
+		}
+		public static void Input(string input)
+		{
+			Console.ForegroundColor = ConsoleColor.DarkCyan;
+			Console.Write("= ");
+			Console.ResetColor();
+			Console.Write(input);
+		}
+		public static void OK(string input)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write("! ");
+			Console.ResetColor();
+			Console.Write(input + Environment.NewLine);
+		}
+		public static void Error(string input)
+		{
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("! ");
+			Console.ResetColor();
+			Console.Write(input + Environment.NewLine);
+		}
+		public static void Do(string input)
+		{
+			Console.ForegroundColor = ConsoleColor.Cyan;
+			Console.Write("- ");
+			Console.ResetColor();
+			Console.Write(input + Environment.NewLine);
+		}
+		public static void SendIn(string input)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.Write("<");
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.Write("-");
+			Console.ResetColor();
+			Console.Write(input + Environment.NewLine);
+		}
+		public static void SendOut(string input)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.Write(">");
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.Write("-");
+			Console.ResetColor();
+			Console.Write(input + Environment.NewLine);
+		}
+	}
+	public class Line
+	{
+		public static void Single()
+		{
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine("---");
+			Console.ResetColor();
+		}
+		public static void Double()
+		{
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.WriteLine("===");
+			Console.ResetColor();
+		}
+	}
+}
