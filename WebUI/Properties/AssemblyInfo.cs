ce9628a89d8972e7a01dfc94f4a41f85dc476c2b
Ducky <mail@ducky.ws>
2014-10-27 13:46:19 +0000

    Cleaned out the crap

--- WebUI/Properties/AssemblyInfo.cs
+++ WebUI/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5f640e56-c605-40ec-bcb7-2820065da1fa")]
+[assembly: Guid("f23ccd8a-a606-44b3-8c9d-cbbe6cd7fe45")]
 
 // Version information for an assembly consists of the following four values:
 //
