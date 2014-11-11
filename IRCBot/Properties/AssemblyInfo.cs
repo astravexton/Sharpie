a79c2c8bbd2f90e3484511902c5c154a2d2abb25
Ducky <mail@ducky.ws>
2014-11-11 22:54:47 +0000

    Outlined versioning. Bumped to 0.3.

--- IRCBot/Properties/AssemblyInfo.cs
+++ IRCBot/Properties/AssemblyInfo.cs
@@ -24,13 +24,13 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+//      Major Version   Major change to the architecture
+//      Minor Version   Add/Removed Plugin / small changes to the architecture
+//      Build Number    Auto
+//      Revision        Auto
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyVersion("0.3.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
