3f3ddf7d3259e17debd478c060cddd29c76c1784
Ducky <mail@ducky.ws>
2014-12-01 02:56:19 +0000

    Thing plugin.

---
+++ IRCBot/Plugins/Things.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRCBot.Plugins
+{
+    class Things
+    {
+        public static void Start()
+        {
+            switch (Global.IRCMessage)
+            {
+                case "lod":
+                    Say.IRC("\u0CA0_\u0CA0");
+                    break;
+            }
+        }
+    }
+}
