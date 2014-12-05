7b1dbc485bdca631ff5cd6bc05d3cb31db39b336
Ducky <mail@ducky.ws>
2014-12-05 15:34:53 +0000

    Let's try a thing

--- IRCBot/Plugins/PornMD.cs
+++ IRCBot/Plugins/PornMD.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Net;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using System.IO;
 
 namespace IRCBot.Plugins
 {
@@ -61,11 +62,12 @@ namespace IRCBot.Plugins
                 recent = "";
             }
 
-            var client = new WebClient();
-            client.Headers.Add("User-Agent", Global.UAS); //my endpoint needs this...
+            //var client = new WebClient();
+            //client.Headers.Add("User-Agent", Global.UAS); //my endpoint needs this...
             try
             {
-                var response = client.DownloadString(new Uri("http://www.pornmd.com/" + orientation + "/" + query + "?start=50&ajax=true&limit=50&format=json" + recent));
+                //var response = client.DownloadString(new Uri("http://www.pornmd.com/" + orientation + "/" + query + "?start=50&ajax=true&limit=50&format=json" + recent));
+				var response = GetFile("http://www.pornmd.com/" + orientation + "/" + query + "?start=50&ajax=true&limit=50&format=json" + recent);
                 var o = JsonConvert.DeserializeObject<JSON.Root>(response);
 
                 switch (o.videos[0].source.ToLower())
@@ -124,6 +126,29 @@ namespace IRCBot.Plugins
             {
             }
         }
+
+		static string GetFile(string strURL)
+		{
+			string strResult;
+			strResult = " ";
+			try
+			{
+				WebResponse objResponse;
+				WebRequest objRequest = HttpWebRequest.Create(strURL);
+				objResponse = objRequest.GetResponse();
+				using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
+				{
+					strResult = sr.ReadToEnd();
+					sr.Close();
+				}
+				strResult.Replace("\n", Environment.NewLine);
+			}
+			catch (Exception e)
+			{
+				strResult = e.ToString();
+			}
+			return strResult;
+		}
 
         public class JSON
         {
